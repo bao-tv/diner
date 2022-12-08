f29c140150e7d9db95673a8d4209a33c7651b0c7
Viet Bao <vietbao.t@gmail.com>
2022-12-09 00:56:38 +0700

    full

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+// ?count up number
+$('.counter__number').counterUp({
+    delay: 10,
+    time: 3000
+  });
+
+
+// wow scoll animation
+new WOW().init(); 
+
+
+// boootstrap slide
+
+$('.carousel-item').carousel()
+
+
