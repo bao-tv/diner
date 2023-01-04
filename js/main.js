912ca1331ecfc0ae2190ced3149f12ce925d6a77
Viet Bao <vietbao.t@gmail.com>
2023-01-05 01:02:38 +0700

    fix menu show

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+
+// ?count up number
+$('.counter__number').counterUp({
+    delay: 10,
+    time: 3000
+  });
+
+
+// wow scoll animation
+new WOW().init(); 
+
+
+// boootstrap slide
+
+$('.carousel-item').carousel()
+
+// myBtnMenu
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("myBtnMenu").onclick = function() {myAddShow()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myAddShow() {
+    document.getElementById("bannerMenu").classList.add("show");
+}
+
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("bannerMenuClose").onclick = function() {myCloseShow()};
+function myCloseShow() {
+  document.getElementById("bannerMenu").classList.remove("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+  
+//   if (!event.target.matches('.dropbtn')) {
+
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+      
+//     for (let i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+// $('.pull-right').click
+$(".pull-right div").click(function(){
+  $(".sidebar").toggleClass('active');
+});
+
+
+// Magnific Popup
+$(document).ready(function () {
+  $("#popup-video").magnificPopup({
+    type: "inline",
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: "auto",
+
+    closeBtnInside: true,
+    preloader: false,
+
+    midClick: true,
+    removalDelay: 300,
+    mainClass: "my-mfp-zoom-in"
+  });
+});
+
+// Dark theme
+document.getElementById("switchButton").onclick = function () {
+  document.getElementById("myBody").classList.toggle("dark");
+};
